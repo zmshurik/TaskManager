df48d99cdee80bc4e955fa87b4559e5d687a3e64
Alexander Kurakov <zmshurik@yandex.ru>
2019-01-26 23:36:34 +0700

    change db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_26_151932) do
+ActiveRecord::Schema.define(version: 2019_01_26_160435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "author_id"
+    t.integer "assignee_id"
+    t.string "state"
+    t.date "expired_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
