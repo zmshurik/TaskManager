595254df83e3359beb94de5b3073a7c3431d35e3
Alexander Kurakov <zmshurik@yandex.ru>
2019-01-29 21:21:45 +0700

    add validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,9 @@
 class Task < ApplicationRecord
     belongs_to :author, class_name: 'User'
     bolongs_to :assignee, class_name: 'User'
+
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :author, presence: true
+    validates :description, length: { maximum: 500 }
 end
