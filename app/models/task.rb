b9067ed8fd753a68f04425e198bac02b9fd24804
Alexander Kurakov <zmshurik@yandex.ru>
2019-01-29 21:46:26 +0700

    fix task association

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  belongs_to :assignee, class_name: 'User'
+  belongs_to :assignee, class_name: 'User', optional: true
 
   validates :name, presence: true
   validates :description, presence: true
