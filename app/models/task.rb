3af7955d784b1c62db7362bbf723c8c95f60653b
Alexander Kurakov <zmshurik@yandex.ru>
2019-01-29 21:42:17 +0700

    fix task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,9 @@
 class Task < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    bolongs_to :assignee, class_name: 'User'
+  belongs_to :author, class_name: 'User'
+  belongs_to :assignee, class_name: 'User'
 
-    validates :name, presence: true
-    validates :description, presence: true
-    validates :author, presence: true
-    validates :description, length: { maximum: 500 }
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :author, presence: true
+  validates :description, length: { maximum: 500 }
 end
