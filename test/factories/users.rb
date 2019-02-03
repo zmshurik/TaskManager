80940b7a156e3c726e1f063f4309f26eebb3550e
Alexander Kurakov <zmshurik@yandex.ru>
2019-02-03 19:55:21 +0700

    fix user factory

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,10 +1,9 @@
 FactoryBot.define do
   factory :user do
-    first_name { "MyString" }
-    last_name { "MyString" }
-    password { "" }
-    email { "MyString" }
-    avatar { "MyString" }
-    type { "" }
+    first_name { generate :string }
+    last_name { generate :string }
+    password { generate :string }
+    email
+    type { "Developer" }
   end
 end
