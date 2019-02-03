e12b9aca2310ae2c061d941b6d778bc9a9f10b49
Alexander Kurakov <zmshurik@yandex.ru>
2019-02-03 19:27:04 +0700

    add user creation

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   scope module: :web do
     resource :board, only: :show
     resource :session, only: :new
+    resources :developers, only: [:new, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
