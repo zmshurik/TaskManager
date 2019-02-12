86b9b76b65a7de826aeb30feddd60c7ebd8f2391
Alexander Kurakov <zmshurik@yandex.ru>
2019-02-12 21:10:15 +0700

    add create and edit actions for users

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -16,4 +16,28 @@ class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
     get admin_users_url
     assert_response :success
   end
+
+  test 'should get edit' do
+    user = create(:user)
+    get edit_admin_user_url user.id
+    assert_response :success
+  end
+
+  test 'should get new' do
+    get new_admin_user_url
+    assert_response :success
+  end
+
+  test 'should post create' do
+    user = attributes_for(:user)
+    post admin_users_url, params: { user: user }
+    assert_response :redirect
+  end
+
+  test 'should patch update' do
+    user = create(:user)
+    user_attrs = attributes_for(:user)
+    patch admin_user_url user.id, params: { user: user_attrs }
+    assert_response :redirect
+  end
 end
