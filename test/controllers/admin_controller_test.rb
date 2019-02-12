ccdc145609a215868dfca58e079b40049878be6d
Alexander Kurakov <zmshurik@yandex.ru>
2019-02-12 20:30:41 +0700

    add authorization

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    admin = create(:admin)
+    sign_in_as admin
+  end
+
   test "should get show" do
     user = create(:user)
     get admin_user_url(user)
