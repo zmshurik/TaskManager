758d99dc48b0b3ea71857d1f5a972a09cfa06f3a
Alexander Kurakov <zmshurik@yandex.ru>
2019-02-27 20:01:05 +0700

    add index action

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::ApplicationController < Api::ApplicationController
+  def build_meta_tasks(collection)
+    {
+      count: collection.count,
+      total_count: collection.total_count,
+      current_page: collection.current_page,
+      total_pages: collection.total_pages,
+      per_page: collection.limit_value
+    }
+  end
 end
--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::TasksController < Api::V1::ApplicationController
+  respond_to :json
+
+  def index
+    q_params = params[:q] || { s: 'id asc' }
+
+    tasks = Task.all
+                .ransack(q_params)
+                .result
+                .page(params[:page])
+                .per(params[:per_page])
+
+    json = {
+      items: tasks.map { |t| TaskSerializer.new(t).as_json },
+      meta: build_meta_tasks(tasks)
+    }
+
+    respond_with json
+  end
 end
