f98b4d41cc64841a105666e9a893484a23629f48
Alexander Kurakov <zmshurik@yandex.ru>
2019-03-11 19:10:38 +0700

    add trello board

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import WebpackerReact from 'webpacker-react';
+import TaskBoard from './components/TaskBoard';
+
+WebpackerReact.setup({ TaskBoard });
