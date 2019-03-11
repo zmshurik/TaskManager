d35d1863451482ec2722687e78f9c4b784063183
Alexander Kurakov <zmshurik@yandex.ru>
2019-03-11 18:41:06 +0700

    install webpacker

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  # Verifies that versions and hashed value of the package contents in the project's package.json
+  config.webpacker.check_yarn_integrity = true
   config.after_initialize do
     Bullet.enable        = true
     Bullet.alert         = true
